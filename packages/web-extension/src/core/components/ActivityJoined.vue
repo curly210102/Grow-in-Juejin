fc2a19843081b24371c25e058fb89d2125abdbae
Curly Brackets <water.curly@outlook.com>
2023-03-04 00:16:53 +0800

    refactor(activity): 增加无已参与活动提示

--- packages/web-extension/src/core/components/ActivityJoined.vue
+++ packages/web-extension/src/core/components/ActivityJoined.vue
@@ -159,8 +159,14 @@ const joinedActivities = computed(() => {
 <template>
     <SectionHeader title="已参与的活动">
     </SectionHeader>
-    <div class="grid gap-2 grid-cols-2">
+    <div class="grid gap-2 grid-cols-2" v-if="joinedActivities.length">
         <ActivityCard v-for="activity in joinedActivities" class="card" :activity="activity">
         </ActivityCard>
+    </div>
+    <div v-else class="border rounded-md border-gray-200/80 bg-gray-100/50 text-slate-400 text-sm text-center p-4">
+        <a href="https://github.com/curly210102/grow-in-juejin/issues/new" target="_blank" class="block">
+            未检测到已参与的活动，如有差异请提交
+            Issue
+        </a>
     </div>
 </template>
