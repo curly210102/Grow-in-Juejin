8291a3a3a2b0ba69aff427d32421aa658da77e02
Curly Brackets <water.curly@outlook.com>
2023-03-09 22:35:23 +0800

    fix(user): 多张活动卡片布局

--- packages/web-extension/src/content-scripts/user/components/ClientJoinedActivity.ce.vue
+++ packages/web-extension/src/content-scripts/user/components/ClientJoinedActivity.ce.vue
@@ -24,6 +24,7 @@ const articleListRef: Ref<IArticle[]> = ref([]);
 const articleContentMapRef: Ref<ArticleContentMap> = ref(new Map());
 const earliestStartTime = computed(() => Math.min(...activitiesRef.value.map(a => a.startTimeStamp ?? Infinity)));
 
+
 watchEffect(async () => {
     if (earliestStartTime.value !== Infinity) {
         const { articleList, articleContentMap } = await initUserArticles(userId, earliestStartTime.value)
@@ -38,8 +39,8 @@ const joinedActivities = useComputeJoinedActivities(activitiesRef,
 
 </script>
 <template>
-    <div class="juejin-card" v-if="joinedActivities.length">
-        <ActivityCard v-for="activity in joinedActivities" :activity="activity">
+    <div v-if="joinedActivities.length" class="space-y-2">
+        <ActivityCard v-for="activity in joinedActivities" :activity="activity" class="juejin-card">
         </ActivityCard>
     </div>
 </template>
