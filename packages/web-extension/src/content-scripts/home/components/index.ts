a616abe0ce947a12c1ca0c4d084146347fe9ed58
CPunisher <1343316114@qq.com>
2023-05-12 11:35:32 +0800

    fix: load webcomponents polyfill

--- packages/web-extension/src/content-scripts/home/components/index.ts
+++ packages/web-extension/src/content-scripts/home/components/index.ts
@@ -1,5 +1,6 @@
 import { defineCustomElement } from 'vue'
 import ClientActivityFloating from './ClientActivityFloating.ce.vue'
+import '@webcomponents/webcomponentsjs/webcomponents-bundle'
 
 const CustomActivityFloating = defineCustomElement(ClientActivityFloating)
 
