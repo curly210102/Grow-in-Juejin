c8dfc5d17a1146c628a8ab4f1919af652e6a234a
Curly Brackets <water.curly@outlook.com>
2023-06-06 10:04:50 +0800

    chore(script): update prefixer

--- packages/web-extension/prefixer.js
+++ packages/web-extension/prefixer.js
@@ -1,57 +1,134 @@
 const fs = require("fs");
-const path = require("path");
+const resolveConfig = require("tailwindcss/lib/public/resolve-config").default;
+const resolveConfigPath =
+  require("tailwindcss/lib/util/resolveConfigPath").default;
+const {
+  createContext,
+  getFileModifiedMap,
+} = require("tailwindcss/lib/lib/setupContextUtils");
+const {
+  parseCandidateFiles,
+  resolvedChangedContent,
+} = require("tailwindcss/lib/lib/content");
+const {
+  defaultExtractor: createDefaultExtractor,
+} = require("tailwindcss/lib/lib/defaultExtractor");
+const { resolveMatches } = require("tailwindcss/lib/lib/generateRules");
+const regex = require("tailwindcss/lib/lib/regex");
+const {
+  splitAtTopLevelOnly,
+} = require("tailwindcss/lib/util/splitAtTopLevelOnly");
 
-const prefix = "gij-";
+const PREFIX = "gij-";
 
-function getAllVueFiles(dirPath, fileList = []) {
-  const files = fs.readdirSync(dirPath);
-
-  files.forEach((file) => {
-    const filePath = path.join(dirPath, file);
-    const stat = fs.statSync(filePath);
-    if (stat.isDirectory()) {
-      getAllVueFiles(filePath, fileList);
-    } else if (path.extname(file) === ".vue") {
-      fileList.push(filePath);
-    }
-  });
-
-  return fileList;
+// 创建 context
+function getContext(tailwindConfig) {
+  return createContext(tailwindConfig);
 }
 
-getAllVueFiles(__dirname).forEach(addPrefixToSingleFile);
+// 获取所有模板文件
+function getAllTemplateFiles(context, tailwindConfig) {
+  let fileModifiedMap = getFileModifiedMap(context);
+  let candidateFiles = parseCandidateFiles(context, tailwindConfig);
+  let [allFiles] = resolvedChangedContent(
+    context,
+    candidateFiles,
+    fileModifiedMap
+  );
+  return allFiles;
+}
 
-function addPrefixToSingleFile(fileName) {
-  fs.readFile(fileName, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
+// 主逻辑
+function processTailwindPrefix() {
+  const tailwindConfig = resolveConfig(require(resolveConfigPath()));
+  const context = getContext(tailwindConfig);
+  const allFiles = getAllTemplateFiles(context, tailwindConfig);
 
-    const regex = /[^:]class="([^"]+)"|:class="[^']*'([^'+])'"/g;
-    let match = regex.exec(data);
-    while (match != null) {
-      const classList = match[1].split(" ");
-      const transformedList = classList.map((item) => {
-        if (item.startsWith(prefix)) {
-          return item;
+  for (let { file, content } of allFiles) {
+    content = file ? fs.readFileSync(file, "utf8") : content;
+    // 匹配+替换生成新内容
+    const newContent = generateNewContent(content, context);
+
+    if (newContent !== content) {
+      fs.writeFile(file, newContent, "utf-8", (err) => {
+        if (err) {
+          console.error(`Update ${file} Failed`);
+        } else {
+          console.log(`Update ${file} Success`);
         }
-        const className = item.split(":")[1] ?? item;
-
-        return className.startsWith("-")
-          ? `-${prefix}${className.slice(1)}`
-          : prefix + className;
       });
-      const transformedString = transformedList.join(" ");
-      data = data.replace(match[1], transformedString);
-      match = regex.exec(data);
     }
-
-    fs.writeFile(fileName, data, "utf8", (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-    });
-  });
+  }
 }
+
+function generateNewContent(content, context) {
+  const defaultExtractor = createDefaultExtractor(context);
+  // 逐行匹配并替换
+  const newContent = content
+    .split("\n")
+    .map((line) => {
+      if (line.trim().length === 0) {
+        return line;
+      }
+      // 提取行中原子 class
+      const candidates = new Set(
+        defaultExtractor(line).filter((s) => s !== "!*")
+      );
+      const classes = Array.from(candidates).filter(
+        (candidate) => Array.from(resolveMatches(candidate, context)).length > 0
+      );
+
+      if (classes.length) {
+        // 将 class 替换为增加了 prefix 的 class
+        const classRegex = regex.pattern([
+          /(?<=^|\s|['"`])/,
+          regex.any(classes.map(regex.escape)),
+          /(?=$|\s|['"`])/,
+        ]);
+        return line.replace(classRegex, (match) => addPrefix(match, context));
+      } else {
+        return line;
+      }
+    })
+    .join("\n");
+  return newContent;
+}
+
+// 增加 Prefix
+// 拆解出 variants, important, class
+// 再组装为 `${variants}${important}${prefix}${class}`
+function addPrefix(tailwindClass, context) {
+  const separator = context.tailwindConfig.separator;
+  let [classCandidate, ...variants] = splitAtTopLevelOnly(
+    tailwindClass,
+    separator
+  ).reverse();
+
+  let important = false;
+  if (classCandidate.startsWith("!")) {
+    important = true;
+    classCandidate = classCandidate.slice(1);
+  }
+
+  let negative = false;
+  if (classCandidate.startsWith("-")) {
+    negative = true;
+    classCandidate = classCandidate.slice(1);
+  }
+
+  // 任意属性不添加前缀
+  if (classCandidate.startsWith("[")) {
+    return tailwindClass;
+  }
+
+  variants.reverse().push("");
+  return (
+    variants.join(separator) +
+    (important ? "!" : "") +
+    (negative ? "-" : "") +
+    PREFIX +
+    classCandidate
+  );
+}
+
+processTailwindPrefix();
